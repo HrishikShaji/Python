02589392d5e843c098619d4d3d76ec7ca37c8a53
hrishik <hrishikminishaji@gmail.com>
2023-02-01 18:50:34 +0530

    rockPaperScissor competed

---
+++ rockPaperScissor.py
@@ -0,0 +1,32 @@
+import random
+
+options = ["rock","paper","scissors"]
+computer_wins = 0
+user_wins = 0
+
+while True:
+    user_input = input("pick Rock/Paper/Scissors or Q to quit").lower()
+    if user_input == "q":
+        print(f"you won {user_wins} times ,computer won {computer_wins} times")
+        break
+    if user_input not in options:
+        print("invalid input")
+        continue
+    if user_input in options:
+        random_number = random.randint(0, 2)
+        computer_pick = options[random_number]
+        print(f"computer picked {computer_pick}")
+        if user_input == "rock" and computer_pick == "scissors":
+            print("you won")
+            user_wins += 1
+        elif user_input == "paper" and computer_pick == "rock":
+            print("you won")
+            user_wins += 1
+        elif user_input == "scissors" and computer_pick == "paper":
+            print("you won")
+            user_wins += 1
+        else:
+            print("you lost")
+            computer_wins += 1
+    else:
+        continue
